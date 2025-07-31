1fa279f75e04c7aca9ff0c2bfaca03ba0848fc26
Luckardo <tolosaalucas@gmail.com>
2025-07-31 10:05:15 -0300

    ligeros retoques

--- Cliente.java
+++ Cliente.java
@@ -5,13 +5,10 @@ import java.util.Random;
 public class Cliente {
 	public Cliente(int sueldo){
 		Inicio i = new Inicio(sueldo);
+		int id = Inicio.getId();
 	}
-	
-	
-	
-	public static void main(String[] args) {
-		Cliente c1 = new Cliente(2);
-		Cliente c2 = new Cliente(3);
-		Cliente c3 = new Cliente(4);
+	private int id;
+	public void gritarAEmpleado() {
+		System.out.println("¡Pero como puede ser que este todo tan caro!¡que verguenza!");
 	}
 }
--- Empleado.java
+++ Empleado.java
@@ -4,7 +4,10 @@ public class Empleado {
 	
 	public Empleado(int sueldo) {
 		Inicio i = new Inicio(sueldo);
-		
+		this.Id = i.getId();
 	}
-	
+	private int Id;
+	public void responderGritos() {
+		System.out.println("Disculpe señora, nosotros no ponemos los precios");
+	}
 }
